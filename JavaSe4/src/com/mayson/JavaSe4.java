6df67caf492a0e1cac12ac5a2365d5d296f81902
Tabsib <525057822@qq.com>
2019-04-19 09:10:54 +0800

    第二次修改

--- JavaSe4/src/com/mayson/JavaSe4.java
+++ JavaSe4/src/com/mayson/JavaSe4.java
@@ -5,7 +5,8 @@ public class JavaSe4 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("JavaSe4");
-		System.out.println("JavaSe5");
+		System.out.println("JavaSe41");
+		System.out.println("JavaSe42");
 	}
 
 }
